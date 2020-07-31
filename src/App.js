30d833fc224e2fab3d2916bebf994e374a496b53
paakowden <paakow1114@gmail.com>
2020-07-31 08:10:57 -0400

    Todo-App with firebase

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TODO APP </h1>
+      <input />
+      <button>Add Todo</button>
+
+      <ul>
+        <li>Take dogs for a walk</li>
+        <li>Take the rubbish out</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDTmxqwKPj9hnlEIoO34a7mq798JcsErVg",
+  authDomain: "todo-app-cp-91209.firebaseapp.com",
+  databaseURL: "https://todo-app-cp-91209.firebaseio.com",
+  projectId: "todo-app-cp-91209",
+  storageBucket: "todo-app-cp-91209.appspot.com",
+  messagingSenderId: "847157336973",
+  appId: "1:847157336973:web:2a3c3229be3661a89c3bc4",
+  measurementId: "G-WJ3X3PWW3L",
+};
